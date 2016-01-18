2aa957374c3d10f3372179bb9defa7cda613aaf8
Invertex <kakkoister@hotmail.com>
2016-01-18 07:55:45 -0800

    Added source and binary

---
+++ BruteU/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace BruteU
+{
+    partial class bruteWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bruteWindow));
+            this.keyInput = new System.Windows.Forms.TextBox();
+            this.hashInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.byteLength = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.startBrute = new System.Windows.Forms.Button();
+            this.save2bin = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.fastMode = new System.Windows.Forms.CheckBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.byteLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // keyInput
+            // 
+            this.keyInput.Location = new System.Drawing.Point(12, 33);
+            this.keyInput.Name = "keyInput";
+            this.keyInput.Size = new System.Drawing.Size(249, 20);
+            this.keyInput.TabIndex = 0;
+            // 
+            // hashInput
+            // 
+            this.hashInput.Location = new System.Drawing.Point(12, 81);
+            this.hashInput.MaxLength = 40;
+            this.hashInput.Name = "hashInput";
+            this.hashInput.Size = new System.Drawing.Size(249, 20);
+            this.hashInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key to brute force:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SHA-1 hash to compare to:";
+            // 
+            // byteLength
+            // 
+            this.byteLength.Location = new System.Drawing.Point(368, 33);
+            this.byteLength.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.byteLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.byteLength.Name = "byteLength";
+            this.byteLength.ReadOnly = true;
+            this.byteLength.Size = new System.Drawing.Size(33, 20);
+            this.byteLength.TabIndex = 4;
+            this.byteLength.TabStop = false;
+            this.byteLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.byteLength.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Unknown values:";
+            // 
+            // labelKey
+            // 
+            this.labelKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey.Location = new System.Drawing.Point(93, 139);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(308, 24);
+            this.labelKey.TabIndex = 7;
+            this.labelKey.Text = "Not running";
+            // 
+            // startBrute
+            // 
+            this.startBrute.Location = new System.Drawing.Point(12, 107);
+            this.startBrute.Name = "startBrute";
+            this.startBrute.Size = new System.Drawing.Size(75, 23);
+            this.startBrute.TabIndex = 8;
+            this.startBrute.Text = "Start";
+            this.startBrute.UseVisualStyleBackColor = true;
+            this.startBrute.Click += new System.EventHandler(this.startBrute_Click);
+            // 
+            // save2bin
+            // 
+            this.save2bin.Enabled = false;
+            this.save2bin.Location = new System.Drawing.Point(12, 136);
+            this.save2bin.Name = "save2bin";
+            this.save2bin.Size = new System.Drawing.Size(75, 23);
+            this.save2bin.TabIndex = 9;
+            this.save2bin.Text = "Save to .bin";
+            this.save2bin.UseVisualStyleBackColor = true;
+            this.save2bin.Click += new System.EventHandler(this.save2bin_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // fastMode
+            // 
+            this.fastMode.AutoSize = true;
+            this.fastMode.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fastMode.Location = new System.Drawing.Point(329, 11);
+            this.fastMode.Name = "fastMode";
+            this.fastMode.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.fastMode.Size = new System.Drawing.Size(72, 16);
+            this.fastMode.TabIndex = 11;
+            this.fastMode.Text = ":Fast Mode";
+            this.fastMode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.fastMode.UseVisualStyleBackColor = true;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo.Location = new System.Drawing.Point(93, 110);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(308, 24);
+            this.labelInfo.TabIndex = 12;
+            // 
+            // bruteWindow
+            // 
+            this.AccessibleDescription = "Disabled the visual feedback of generated keys.";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 170);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.fastMode);
+            this.Controls.Add(this.save2bin);
+            this.Controls.Add(this.startBrute);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.byteLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hashInput);
+            this.Controls.Add(this.keyInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "bruteWindow";
+            this.Text = "BruteU - WiiU key brute forcer by Kakkoii";
+            ((System.ComponentModel.ISupportInitialize)(this.byteLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyInput;
+        private System.Windows.Forms.TextBox hashInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown byteLength;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button startBrute;
+        private System.Windows.Forms.Button save2bin;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        public System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.CheckBox fastMode;
+        private System.Windows.Forms.Label labelInfo;
+    }
+}
+
---
+++ BruteU/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace BruteU
+{
+    public partial class bruteWindow : Form
+    {
+        private static string[] hexVal = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" }; //All possible HEX values to use
+        private static byte[] userSHA1; //User defined SHA1 to compare to
+        private string tempKey; //For storing our key variations
+        private string infoText; //For storing info given to the user
+        private bool foundKey = false; //To stop our recursive function from continuing on once we've found our key
+        private bool working = false; //To easily determine what things can happen based on if the brute forcing thread is running
+        private SHA1 sha1 = SHA1.Create(); //Import SHA1 functions to generate SHA-1 hashes of our tempKeys
+
+        public bruteWindow()
+        {
+            InitializeComponent();
+        }
+
+//Begin the bruteforcing when the user clicks Start
+        private void startBrute_Click(object sender, EventArgs e)
+        {
+            if(startBrute.Text == "Stop") //If the Start button is currently configured as 'Stop', we should stop the operations instead.
+            {
+                working = false; //Set working to false, so the conditional in our recursive brute forcing function triggers to stop itself
+                startBrute.Enabled = false; //disable this button until we're sure the backgroundWorker is done
+                while (backgroundWorker1.CancellationPending){} //Don't continue until the backgroundWorker is done
+                labelKey.Text = ""; //Clear previous value
+                labelInfo.Text = "Brute forcing halted."; //We don't need to show any text here if we stopped
+                startBrute.Enabled = true; //Re-enable our button now that we know it's safe to Start it again
+                startBrute.Text = "Start"; //Change the text back to start
+            }
+            else if (hashInput.Text.Length != 40) //Check to make sure user inputted SHA-1 hash is equal to 40, the proper length of a SHA-1.
+            {
+                labelInfo.Text = "Your SHA - 1 hash is not 40 characters long";
+            }
+            else
+            {
+                userSHA1 = Enumerable.Range(0, hashInput.Text.Length / 2).Select(x => Convert.ToByte(hashInput.Text.Substring(x * 2, 2), 16)).ToArray(); //Convert the user defined SHA-1 from a String to a Byte array so we can utilize it properly
+                foundKey = false; //Make sure this variable is set to false to not stop our program prematurely if it had already been ran once.
+                startBrute.Text = "Stop";
+                labelKey.Text = ""; 
+                labelInfo.Text = "Brute forcing..."; //Let the user know what's happening
+                working = true;
+                save2bin.Enabled = false; //Make sure save2bin is disabled if another brute force operation is executed
+                backgroundWorker1.RunWorkerAsync(); //Start our work on a separate thread for better performance. This will trigger the 'backgroundWorker1_DoWork()' method below.
+            }
+        }
+//Work to do
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _BruteKey( (int)byteLength.Value, "", keyInput.Text.ToLower(), hashInput.Text.ToLower()); //Call our method that has all the work we want to do.
+        }
+//Communicates between the threads and triggers RunWorkerCompleted() when Progress reaches 100
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+        }
+//When our main work has completed or been stopped, these operations will be called
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker1.CancelAsync(); //Kill off our thread to make sure it's dead
+            backgroundWorker1.Dispose(); //Dispose of any garbage
+            startBrute.Text = "Start"; //Since our backgroundWorker is done, change our Start button back from Stop.
+            if(foundKey)
+            {
+                save2bin.Enabled = true; //Enable the save2bin button so the user can save the key to a file
+            }
+            else
+            {
+                infoText = "No match found!";
+                UpdateInfoLabel();
+            }
+        }
+
+//Method that does all the brute forcing
+        private void _BruteKey(int bytes, string combo, string key, string hash)
+        {
+            tempKey = key + combo; //Store the current generated key
+
+            if (_SHA1Comp(tempKey)) //Calculate the hash of the currently generated key and compare it to the user provided hash. Or exit out if working has become untrue.
+            {
+                UpdateKeyLabel();
+                infoText = "Match found!";
+                UpdateInfoLabel();
+                working = false; 
+                foundKey = true;
+                backgroundWorker1.ReportProgress(100, "Done!"); //Report our progress as 100 to trigger the RunWorkerCompleted() method.
+            }
+
+            else if (combo.Length == bytes) //If the combination is the same length as extra bytes we're missing, continue
+            {
+                tempKey = key + combo;
+
+                if (!fastMode.Checked) //If Fast Mode isn't checked, we update the label with each generated key to make things interesting
+                {
+                    UpdateKeyLabel();
+                }
+                return;
+            }
+
+            for (int i = 0; i < hexVal.Length; i++) //For each possible HEX value, we need to do this operation
+            {
+                if (foundKey || !working)
+                {
+                    backgroundWorker1.ReportProgress(100, "Done!");
+                    break;
+                }
+                string t = combo;
+                t += hexVal[i];
+                _BruteKey(bytes, t, key, hash);
+            }
+        }
+
+        private bool _SHA1Comp(string s)
+        {
+            byte[] bytes = Enumerable.Range(0, s.Length / 2).Select(x => Convert.ToByte(s.Substring(x * 2, 2), 16)).ToArray(); //Convert our HEX String to Bytes so we can work with it properly
+            byte[] hashBytes = sha1.ComputeHash(bytes); //Compute the SHA-1 of those Bytes
+
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                if (userSHA1[i] != hashBytes[i]) //Compare each Byte in both hashes, if there is a single mismatch, return false
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void UpdateKeyLabel()
+        {
+            //Switch threads if cross-thread call would happen
+            if (InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(UpdateKeyLabel);
+                Invoke(method);
+                return;
+            }
+            labelKey.Text = tempKey;
+        }
+
+        private void UpdateInfoLabel()
+        {
+            //Switch threads if cross-thread call would happen
+            if (InvokeRequired)
+            {
+                MethodInvoker method = new MethodInvoker(UpdateInfoLabel);
+                Invoke(method);
+                return;
+            }
+            labelInfo.Text = infoText;
+        }
+
+        private void save2bin_Click(object sender, EventArgs e)
+        {
+            byte[] keyBytes = Enumerable.Range(0, tempKey.Length / 2).Select(x => Convert.ToByte(tempKey.Substring(x * 2, 2), 16)).ToArray(); //Convert our found key to bytes so we can save it properly
+            string filename = "key"; 
+
+            using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+            {
+                saveFileDialog1.FileName = filename;
+                saveFileDialog1.Filter = "Binary File | *.bin"; //Sets the default file type we want
+                if (DialogResult.OK != saveFileDialog1.ShowDialog()) //Bring up a save dialogue to choose where to save
+                    return;
+                File.WriteAllBytes(saveFileDialog1.FileName,keyBytes); //Write all the bytes of our key to that location
+            }
+        }
+    }
+}
